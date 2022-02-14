c128d50617159490463a2f533d39f7a008a07d5d
Ayush Bajpai <60637499+Ayushbajpai19@users.noreply.github.com>
2022-02-14 15:36:06 +0530

    Create will.sol

---
+++ will.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.5.7;
+
+contract Will {
+    address owner;
+    uint fortune;
+    bool deceased;
+    
+    constructor() payable public {
+        owner = msg.sender;
+        fortune = msg.value;
+        deceased = false;
+
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+
+    }
+
+    modifier mustBeDeceased {
+        require(deceased == true);
+        _;
+        
+    }
+    address payable[] familyWallets;
+
+    mapping( address => uint) inheritance;
+
+    function setInheritance(address payable wallet, uint amount) public
+    {
+    familyWallets.push(wallet);
+    inheritance[wallet] = amount;
+
+    }
+
+    function payout() private mustBeDeceased {
+        for(uint i = 0; i< familyWallets.length; i++){
+            familyWallets[i].transfer(inheritance[familyWallets[i]]);
+        }
+    }
+
+    function hasDeceased() public onlyOwner {
+        deceased = true;
+        payout();
+    }
+
+}
